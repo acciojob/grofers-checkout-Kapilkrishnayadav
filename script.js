04fee17c3e719b3d23af6c5305396c58bf9d7df7
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-06-01 20:14:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	console.log("hello")
+	const prices=document.querySelectorAll(".price");
+    let sum=0;
+    for(let i=0;i<prices.length;i++)
+{
+    sum+=Number(prices[i].innerText);
+}
+const totalSum=document.querySelector("table");
+totalSum.innerHTML+=`<tr>
+      <td>${sum}</td>
+    </tr>`
   
 };
 
